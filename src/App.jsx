af9679a85072cd35b969ecc7b06d29a76116faae
Zubia <rashzubia@gmail.com>
2022-07-17 20:27:13 +0200

    fetched local data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import axios from 'axios';
+
+const backend_url = import.meta.env.VITE_BACKEND_URL 
+
 
 function App() {
+  const [linkList, setLinkList] = useState([])
 
-
+useEffect(() => {
+  (async () => {
+    setLinkList((await axios.get(backend_url)).data);
+  })()
+}, [])
   return (
     <div className="App">
-     
+     <h1>Zubi's Webdev Links</h1>
+     <p>There are {linkList.length} links</p>
+     <ul>
+                                    {linkList.map((linkList, i) => {
+                                        return <a key={i} href={linkList.url} target="_blank"><li key={i}>{linkList.title}</li></a>;
+                                    })}
+                                </ul>
     </div>
   )
 }
