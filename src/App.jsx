f70c51ea34605766f5243ad024e128320b7eb164
Zubia <rashzubia@gmail.com>
2022-07-19 18:28:48 +0200

    ready for jwt

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,95 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
-const backend_url = import.meta.env.VITE_BACKEND_URL 
-
+const backend_url = import.meta.env.VITE_BACKEND_URL;
+const backend_base_url = "http://localhost:3060";
 
 function App() {
-  const [linkList, setLinkList] = useState([])
+    const [linkList, setLinkList] = useState([]);
+    const [currentUser, setCurrentUser] = useState({});
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
 
-useEffect(() => {
-  (async () => {
-    setLinkList((await axios.get(backend_url)).data);
-  })()
-}, [])
-  return (
-    <div className="App">
-     <h1>Zubi's Webdev Links</h1>
-     <p>There are {linkList.length} links</p>
-     <ul>
-                                    {linkList.map((linkList, i) => {
-                                        return <a key={i} href={linkList.url} target="_blank"><li key={i}>{linkList.title}</li></a>;
-                                    })}
-                                </ul>
-    </div>
-  )
+    const userIsLoggedIn = () => {
+        return Object.keys(currentUser).length > 0;
+    };
+
+    const getLinklist = () => {
+        (async () => {
+            setLinkList(
+                (await axios.get(backend_base_url + "/linkLists")).data
+            );
+        })();
+    };
+
+    useEffect(() => {
+        if (userIsLoggedIn()) {
+            getLinklist();
+        }
+    }, []);
+
+    const handleLoginButton = async (e) => {
+      e.preventDefault();
+      const response = await fetch(backend_base_url + '/login', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ username, password })
+      });
+      setUsername('');
+      setPassword('');
+      if (response.ok) {
+          const data = await response.json();
+          getLinklist();
+          setCurrentUser(data.user);
+      } else {
+          setMessage('bad login');
+      }
+  };
+   
+    return (
+        <div className="App">
+            <h1>Zubi's Webdev Links</h1>
+            {userIsLoggedIn() ? (
+                <>
+                    <p>There are {linkList.length} links</p>
+                    <ul>
+                        {linkList.map((linkList, i) => {
+                            return (
+                                <a key={i} href={linkList.url} target="_blank">
+                                    <li key={i}>{linkList.title}</li>
+                                </a>
+                            );
+                        })}
+                    </ul>
+                </>
+            ) : (
+                <form className="login" onSubmit={handleLoginButton}>
+                    <div className="row">
+                        username:{" "}
+                        <input
+                            onChange={(e) => setUsername(e.target.value)}
+                            value={username}
+                            type="text"
+                        />
+                    </div>
+                    <div className="row">
+                        password:{" "}
+                        <input
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                            type="password"
+                        />
+                    </div>
+                    <div className="row">
+                        <button>Login</button>
+                    </div>
+                    <div className="row">{message}</div>
+                </form>
+            )}
+        </div>
+    );
 }
 
-export default App
+export default App;
